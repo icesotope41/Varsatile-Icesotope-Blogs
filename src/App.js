19ecfc25bc02a91e62b98edaf6ea192693bc70bb
Icesotope <53615543+AbsoluteZeroist@users.noreply.github.com>
2021-07-25 21:52:28 +0800

    send help

--- src/App.js
+++ src/App.js
@@ -1,17 +1,20 @@
-
 import './App.css';
 
 function App() {
-  return (
-    <div>
-      <h1>Hello!</h1>
-      <p>Congratulations! you have reached the pre-production 
-        stages of my website! If you want to be a member of Incon 
-        you simply just need to help commit into the repository 
-        <a href="">Varsatile-Icesotope-Blogs</a>
-      </p>
-    </div>
-  );
+    return (
+        <div class="screen">
+            <h1>Hello!</h1>
+            <p>
+                Congratulations! you have reached the pre-production 
+                stages of my website! If you want <br/> to be a member of Incon 
+                you simply just need to help commit into the repository <br/>
+                <a class='goRepoBtn' href="https://github.com/icesotope41/Varsatile-Icesotope-Blogs" 
+                target="_blank">
+                    Varsatile-Icesotope-Blogs
+                </a> 
+            </p>
+        </div>
+    );
 }
 
 export default App;
