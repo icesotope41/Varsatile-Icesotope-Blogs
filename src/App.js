b081726f7ee25b122e6d84cbaa65d4e6ee472609
Icesotope <53615543+AbsoluteZeroist@users.noreply.github.com>
2021-07-25 23:47:07 +0800

    added user component
    just testing some stuff out

--- src/App.js
+++ src/App.js
@@ -1,14 +1,19 @@
 import './App.css';
 
+const Greet = (prop) => {
+   return(<h1>Hello, {prop.name}!</h1>);
+}
+
 function App() {
     return (
         <div class="screen">
-            <h1>Hello!</h1>
+            <Greet name="User"/>
             <p>
-                Congratulations! you have reached the pre-production 
-                stages of my website! If you want <br/> to be a member of Incon 
-                you simply just need to help commit into the repository <br/>
-                <a class='goRepoBtn' href="https://github.com/icesotope41/Varsatile-Icesotope-Blogs" 
+                Congratulations! you have reached the pre-production stages of my website! If you want <br/> 
+                to be a member of Incon you simply just need to help commit into the repository <br/>
+                <a 
+                class='goRepoBtn' 
+                href="https://github.com/icesotope41/Varsatile-Icesotope-Blogs" 
                 target="_blank">
                     Varsatile-Icesotope-Blogs
                 </a> 
